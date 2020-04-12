1a18b80248c3c0290a23264e128fe38238cf73ab
Zachary Zensen <TechnomagezZ@yahoo.com>
2020-04-12 10:01:08 -0700

    Updated to add a decrypt function

--- test/exercises/EncryptorTest.java
+++ test/exercises/EncryptorTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class EncryptorTest {
+	String input = "";
+	String[] sentence = new String[6];
 
 	@Test
 	void testEncryptOneCharacter() {
@@ -19,5 +21,25 @@ class EncryptorTest {
 	
 	// Exercise: decrypt this sentence: Nkxg"nqpi"cpf"rtqurgt#
 	
+	@Test
+	void testDecryptWord() throws Exception {
+		
+		String input = "Nkxg nqpi cpf rtqurgt#";
+		
+		
+		assertEquals("aaa", Encryptor.decrypt("ccc"));
+		assertEquals("Java", Encryptor.decrypt("Lcxc"));
+		
+		
+		Encryptor.decrypt(input);
+			
+			System.out.print(Encryptor.decrypt(input).toString());
+			
+	}
+	
+	public static void main(String args[]) {
+		
+		
+	}
 	
 }
