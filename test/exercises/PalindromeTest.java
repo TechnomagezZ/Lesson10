41398b7dccdc727d5e48265bb019cbe3be180fa2
Zachary Zensen <TechnomagezZ@yahoo.com>
2020-04-12 10:02:32 -0700

    Updated to test if palindrom class operates correctly

--- test/exercises/PalindromeTest.java
+++ test/exercises/PalindromeTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 class PalindromeTest {
 	
 	Palindrome testPalindrome = new Palindrome();
+	String word = "";
 
 	@Test
 	void testLoadWords() {
@@ -14,7 +15,15 @@ class PalindromeTest {
 	}
 	
 	// 2. Test that a word exists in the dictionary 
+	@Test
+	void testWordExists() throws Exception {
+		assertTrue(testPalindrome.wordExsits().contains("orange"));
+	}
 	
 	// 3. Test that a word is a palindrome
-
+	@Test
+	void testIsPalindrome() throws Exception {
+		assertTrue(testPalindrome.isPalindrome("Abba"));
+		assertFalse(testPalindrome.isPalindrome("Milk"));
+	}
 }
