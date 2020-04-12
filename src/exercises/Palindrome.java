6f142f244e51713a676e6aa26a1ade593c174f69
Zachary Zensen <TechnomagezZ@yahoo.com>
2020-04-12 09:52:57 -0700

    Found if a word is a palindrome

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -2,11 +2,31 @@ package exercises;
 
 import java.util.List;
 
+
+
 import examples.FileHelper;
 
 public class Palindrome {
-
+	
 	public List<String> loadWords() {
 		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
 	}
+
+	public List<String> wordExsits() {
+		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
+	}
+	
+	public boolean isPalindrome(String word) {
+		String reverse = new StringBuffer(word).reverse().toString();
+		boolean palindrome;
+		if(word.equalsIgnoreCase(reverse)) {
+			palindrome = true;
+		}
+		else {
+			palindrome = false;
+		}
+		return palindrome;
+	}
+	
+	
 }
